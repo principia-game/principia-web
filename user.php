a32732df3435aee932b040cec82b7a105e107a98
ROllerozxa <temporaryemail4meh+github@gmail.com>
2020-02-24 20:00:02 +0100

    Add URL rewriting for user pages. (CSS is broken atm)

--- user.php
+++ user.php
@@ -2,12 +2,17 @@
 include('lib/common.php');
 pageheader();
 
-$uid = (isset($_GET['id']) ? $_GET['id'] : null);
-
-$userpagedata = fetch("SELECT * FROM users WHERE id = ?", [$uid]);
+if (isset($_GET['id'])) {
+	$userpagedata = fetch("SELECT * FROM users WHERE id = ?", [$_GET['id']]);
+} else if (isset($_GET['name'])) {
+	$userpagedata = fetch("SELECT * FROM users WHERE name = ?", [$_GET['name']]);
+} else {
+	// todo: we should have a error function
+	die("no user specified");
+}
 
 $levels = query("SELECT l.id id,l.title title,u.id u_id,u.name u_name FROM levels l JOIN users u ON l.author = u.id WHERE l.author = ? ORDER BY l.id DESC LIMIT 5",
-	[$uid]);
+	[$userpagedata['id']]);
 
 printf('<h2>%s</h2>', $userpagedata['name']);
 

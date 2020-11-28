b83c3eb371cfbb7a5cc38b365cede24a7cadccfb
ROllerozxa <temporaryemail4meh+github@gmail.com>
2020-11-28 22:09:41 +0100

    Add search page.

---
+++ search.php
@@ -0,0 +1,19 @@
+<?php
+require('lib/common.php');
+
+$query = (isset($_GET['query']) ? $_GET['query'] : '');
+$page = (isset($_GET['page']) ? $_GET['page'] : 1);
+
+$limit = sprintf("LIMIT %s,%s", (($page - 1) * $lpp), $lpp);
+$levels = query("SELECT l.id id,l.title title,u.id u_id,u.name u_name FROM levels l JOIN users u ON l.author = u.id WHERE title LIKE CONCAT('%', ?, '%') ORDER BY l.id DESC $limit",
+	[$query]);
+$count = result("SELECT COUNT(*) FROM levels l WHERE title LIKE CONCAT('%', ?, '%')",
+	[$query]);
+
+$twig = twigloader();
+echo $twig->render('search.twig', [
+	'query' => $query,
+	'page' => $page,
+	'levels' => $levels,
+	'level_count' => $count
+]);

5fc80a90dd87a14b52d4a8a405d86c1bd8afe52b
ROllerozxa <temporaryemail4meh+github@gmail.com>
2020-11-28 22:10:53 +0100

    Change top levels page to limit to the configured LPP.

--- top.php
+++ top.php
@@ -1,7 +1,7 @@
 <?php
 require('lib/common.php');
 
-$levels = query("SELECT l.id id,l.title title,l.likes likes,u.id u_id,u.name u_name FROM levels l JOIN users u ON l.author = u.id ORDER BY l.likes DESC, l.id DESC LIMIT 50");
+$levels = query("SELECT l.id id,l.title title,l.likes likes,u.id u_id,u.name u_name FROM levels l JOIN users u ON l.author = u.id ORDER BY l.likes DESC, l.id DESC LIMIT $lpp");
 
 $twig = twigloader();
 echo $twig->render('top.twig', ['levels' => fetchArray($levels)]);

f5476e3524f8e3736488cd47dd5f645e1d969232
ROllerozxa <temporaryemail4meh+github@gmail.com>
2020-12-24 20:30:32 +0100

    Restrict some internal stuff, add custom 403 page.

--- 404.php
+++ 404.php
@@ -1,4 +1,14 @@
 <?php
 require('lib/common.php');
 
-echo error('404', "The requested page wasn't found.");
+$error = (isset($_GET['error']) ? $_GET['error'] : 404);
+
+switch ($error) {
+	case 403:
+		echo error('403', "Hey, no touching! No looking either!");
+	break;
+	case 404:
+	default:
+		echo error('404', "The requested page wasn't found.");
+	break;
+}

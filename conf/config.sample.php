ee61eda609aea3c6734dbc7e4af11d61c39d30d9
ROllerozxa <temporaryemail4meh+github@gmail.com>
2020-12-29 13:59:59 +0100

    Add a security question to the register page.

--- conf/config.sample.php
+++ conf/config.sample.php
@@ -11,5 +11,20 @@ $tplNoCache = false; // **DO NOT SET AS TRUE IN PROD - DEV ONLY**
 
 $lpp = 20;
 
+// Disable this if you don't have a sense of humor.
+$iHaveASenseOfHumor = true;
+
+// principia-web's CAPTCHA system relies on a list of security questions. It's highly recommended to change these.
+// The answer checking is case-insensitive, but it has to be written in all lowercase here.
+$captcha = [
+	1 => [
+		'question' => 'Are you a bot?',
+		'answer' => ['no']
+	], 2 => [
+		'question' => 'Are you a human?',
+		'answer' => ['yes']
+	]
+];
+
 // Stub function to put special information in the footer.
 function customInfo() { }
--- register.php
+++ register.php
@@ -8,12 +8,15 @@ if (isset($_POST['action'])) {
 	$mail = (isset($_POST['mail']) ? $_POST['mail'] : null);
 	$pass = (isset($_POST['pass']) ? $_POST['pass'] : null);
 	$pass2 = (isset($_POST['pass2']) ? $_POST['pass2'] : null);
+	$captchaId = (isset($_POST['uwu']) ? $_POST['uwu'] : null);
+	$captchaAnswer = (isset($_POST['jupiter']) ? $_POST['jupiter'] : null);
 
 	if (!isset($name)) $error .= 'Blank username. ';
 	if (!isset($mail)) $error .= 'Blank email. ';
 	if (!isset($pass) || strlen($pass) < 6) $error .= 'Password is too short. ';
 	if (!isset($pass2) || $pass != $pass2) $error .= "The passwords don't match. ";
 	if (result("SELECT COUNT(*) FROM users WHERE name = ?", [$name])) $error .= "Username has already been taken.";
+	if (!in_array($captchaAnswer, $captcha[$captchaId]['answer'])) $error .= "Wrong security question answer.";
 
 	if ($error == '') {
 		query("INSERT INTO users (name, password, email) VALUES (?,?,?)", [$name,password_hash($pass, PASSWORD_DEFAULT), $mail]);
@@ -22,5 +25,9 @@ if (isset($_POST['action'])) {
 	}
 }
 
+$chosenCaptcha = array_rand($captcha);
+$currentCaptcha = $captcha[$chosenCaptcha];
+$currentCaptcha['id'] = $chosenCaptcha;
+
 $twig = twigloader();
-echo $twig->render('register.twig', ['error' => $error]);
+echo $twig->render('register.twig', ['error' => $error, 'current_captcha' => $currentCaptcha]);

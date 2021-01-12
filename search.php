980a06afaba078d38b12aeea188639e42695ff02
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-01-12 18:50:57 +0100

    Fix search page.

--- search.php
+++ search.php
@@ -5,7 +5,7 @@ $query = (isset($_GET['query']) ? $_GET['query'] : '');
 $page = (isset($_GET['page']) && is_numeric($_GET['page']) && $_GET['page'] > 0 ? $_GET['page'] : 1);
 
 $limit = sprintf("LIMIT %s,%s", (($page - 1) * $lpp), $lpp);
-$levels = query("SELECT l.id id,l.title title,l.locked locked,u.id u_id,u.name u_name FROM levels l JOIN users u ON l.author = u.id WHERE title LIKE CONCAT('%', ?, '%') AND locked = 0 ORDER BY l.id DESC $limit",
+$levels = query("SELECT l.id id,l.title title,l.locked locked,u.id u_id,u.name u_name FROM levels l JOIN users u ON l.author = u.id WHERE l.title LIKE CONCAT('%', ?, '%') AND l.locked = 0 ORDER BY l.id DESC $limit",
 	[$query]);
 $count = result("SELECT COUNT(*) FROM levels l WHERE title LIKE CONCAT('%', ?, '%')",
 	[$query]);

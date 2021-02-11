76a4780f8b72db843a313141836bce6b3a11675b
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-02-11 21:25:30 +0100

    Add error checking to login page.

--- login.php
+++ login.php
@@ -9,15 +9,25 @@ if (isset($_POST['logout'])) {
 
 if ($log) redirect('./');
 
-if (isset($_POST['action'])) {
-	$logindata = fetch("SELECT id,password FROM users WHERE name = ?", [$_POST['name']]);
+$error = '';
 
-	if (password_verify($_POST['pass'], $logindata['password'])) {
+if (isset($_GET['resetted'])) $error .= 'Password successfully reset! Please login with your new password.';
+
+if (isset($_POST['action'])) {
+	$name = (isset($_POST['name']) ? $_POST['name'] : null);
+	$pass = (isset($_POST['pass']) ? $_POST['pass'] : null);
+
+	$logindata = fetch("SELECT id,password FROM users WHERE name = ?", [$name]);
+
+	if (!$name || !$pass || !$logindata || !password_verify($pass, $logindata['password'])) $error .= 'Invalid credentials.';
+
+	if ($error == '') {
 		setcookie('user', $logindata['id'], 2147483647);
-		setcookie('passenc', base64_encode($_POST['pass']), 2147483647);
+		setcookie('passenc', base64_encode($pass), 2147483647);
+
+		redirect('./');
 	}
-	redirect('./');
 }
 
 $twig = twigloader();
-echo $twig->render('login.twig');
+echo $twig->render('login.twig', ['error' => $error]);

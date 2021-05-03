db3a491515715347101a0ca522517a36d318eb77
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-05-03 12:28:14 +0200

    Add footer information about principia-web and the commit it is running on.

--- lib/misc.php
+++ lib/misc.php
@@ -24,3 +24,18 @@ HTML
 		, $name));
 	}
 }
+
+/**
+ * Get hash of latest git commit
+ *
+ * @param bool $trim Trim the hash to the first 7 characters
+ * @return void
+ */
+function gitCommit($trim = true) {
+	$commit = file_get_contents('.git/refs/heads/master');
+
+	if ($trim)
+		return substr($commit, 0, 7);
+	else
+		return rtrim($commit);
+}

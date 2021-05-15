ae175638d34660fcaf32f843d1b75c509f73782e
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-05-15 15:25:59 +0200

    Split up markdown twig function into several variants with each behavior, add markdown formatting to comments.

--- lib/twig.php
+++ lib/twig.php
@@ -20,10 +20,27 @@ class PrincipiaExtension extends \Twig\Extension\AbstractExtension {
 	public function getFilters() {
 		return [
 			new \Twig\TwigFilter('cat_to_type', 'cat_to_type'),
+
+			// Markdown function for non-inline text, sanitized.
 			new \Twig\TwigFilter('markdown', function ($text) {
+				$markdown = new Parsedown();
+				$markdown->setSafeMode(true);
+				return $markdown->text($text);
+			}, ['is_safe' => ['html']]),
+
+			// Markdown function for inline text, sanitized.
+			new \Twig\TwigFilter('markdown_inline', function ($text) {
+				$markdown = new Parsedown();
+				$markdown->setSafeMode(true);
+				return $markdown->line($text);
+			}, ['is_safe' => ['html']]),
+
+			// Markdown function for non-inline text. **NOT SANITIZED, DON'T LET IT EVER TOUCH USER INPUT**
+			new \Twig\TwigFilter('markdown_unsafe', function ($text) {
 				$markdown = new Parsedown();
 				return $markdown->text($text);
 			}, ['is_safe' => ['html']]),
+
 			new \Twig\TwigFilter('relative_time', 'relativeTime')
 		];
 	}

94fac22c886a954d950bdabee9a17b8ff4d167c5
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-06-10 13:28:17 +0200

    Add script to generate a Gource log from uploaded levels.

---
+++ tools/generate-gourcelog-levels.php
@@ -0,0 +1,20 @@
+#!/usr/bin/php
+<?php
+
+print("principia-web tools - generate-gourcelog-levels.php\n");
+print("===================================================\n");
+print("This script generates a gource log for all levels that have been uploaded to principia-web.\n");
+
+require('lib/common.php');
+
+function skriv($text) {
+	file_put_contents('gource.log', $text . PHP_EOL, FILE_APPEND);
+}
+
+$levels = query("SELECT $userfields l.id,l.title,l.time FROM levels l JOIN users u ON l.author = u.id ORDER BY l.id ASC");
+
+while ($level = $levels->fetch()) {
+	skriv(sprintf(
+		"%s|%s|%s|%s",
+	$level['time'], $level['u_name'], 'A', $level['title']));
+}

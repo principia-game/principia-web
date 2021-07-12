e1cca4fe17965af6ef80554c3c9d4adcf5fe1a7a
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-07-12 19:28:01 +0200

    Update sassc compilation script.

--- tools/compile-scss-sassc.sh
+++ tools/compile-scss-sassc.sh
@@ -3,3 +3,4 @@
 common_arguments="--style compressed --load-path ./"
 
 sassc ${common_arguments} assets/scss/style.scss assets/css/style.css
+sassc ${common_arguments} assets/scss/darkmode.scss assets/css/darkmode.css

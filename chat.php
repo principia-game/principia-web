7d2161f7dae76cbbb9d71416dd3c2236a8bcc238
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-08-16 12:29:47 +0200

    Only show 50 latest messages in chat.

--- chat.php
+++ chat.php
@@ -1,7 +1,7 @@
 <?php
 require('lib/common.php');
 
-$comments = query("SELECT $userfields c.* FROM comments c JOIN users u ON c.author = u.id WHERE c.type = 5 ORDER BY c.time DESC");
+$comments = query("SELECT $userfields c.* FROM comments c JOIN users u ON c.author = u.id WHERE c.type = 5 ORDER BY c.time DESC LIMIT 50");
 
 $twig = twigloader();
 echo $twig->render('chat.twig', ['comments' => $comments]);

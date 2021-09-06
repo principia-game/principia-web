1428a61c2a9df62e8556ca779dcf7842ef56c11e
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-09-06 20:50:13 +0200

    Don't manually print out the JSON In levels_with_no_thumbs.php.

--- internal/levels_with_no_thumbs.php
+++ internal/levels_with_no_thumbs.php
@@ -10,8 +10,6 @@ if (substr($_SERVER['REMOTE_ADDR'],0,8) != "192.168.") {
 
 header('Content-Type: application/json');
 
-echo '{ "levels_with_no_thumbs": [';
-
 $lastlevel = result("SELECT id FROM levels ORDER BY id DESC LIMIT 1");
 
 $levels = [];
@@ -21,11 +19,4 @@ for ($i = 1; $i <= $lastlevel; $i++) {
 	}
 }
 
-$out = '';
-foreach ($levels as $level) {
-	if ($out) $out .= ',';
-	$out .= $level;
-}
-echo $out;
-
-echo '] }';
+echo json_encode(['levels_with_no_thumbs' => $levels]);

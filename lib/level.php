fe5d891d97fc3578c71263eadc2e42ff3f046986
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-09-06 19:40:48 +0200

    Change some switches to matches since we're PHP 8 now.

--- lib/level.php
+++ lib/level.php
@@ -1,45 +1,38 @@
 <?php
 
-$lvl_example = [
-	'id' => 1,
-	'title' => 'Example Level',
-	'u_id' => 1,
-	'u_name' => 'null',
-];
-
 function type_to_cat($type) {
-	switch ($type) {
-		case 'custom':		return 1;
-		case 'adventure':	return 2;
-		case 'puzzle':		return 3;
-	}
+	return match ($type) {
+		'custom'	=> 1,
+		'adventure'	=> 2,
+		'puzzle'	=> 3
+	};
 }
 
 function cat_to_type($cat) {
-	switch ($cat) {
-		case 1:		return 'custom';
-		case 2:		return 'adventure';
-		case 3:		return 'puzzle';
-	}
+	return match ($cat) {
+		1 => 'custom',
+		2 => 'adventure',
+		3 => 'puzzle'
+	};
 }
 
 function catConvert($cat) {
-	switch ((int)$cat) {
-		case 0:		return 3;
-		case 1:		return 2;
-		case 2:		return 1;
-	}
+	return match ($cat) {
+		0 => 3,
+		1 => 2,
+		2 => 1,
+	};
 }
 
 function cmtTypeToNum($type) {
-	switch ($type) {
-		case 'level':	return 1;
-		case 'news':	return 2;
-		case 'contest':	return 3;
-		case 'user':	return 4;
-		case 'chat':	return 5;
-		case 'package':	return 6;
-	}
+	return match ($type) {
+		'level'		=> 1,
+		'news'		=> 2,
+		'contest'	=> 3,
+		'user'		=> 4,
+		'chat'		=> 5,
+		'package'	=> 6
+	};
 }
 
 /**

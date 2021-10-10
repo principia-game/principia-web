2c77483c625a4124f23f8a21671f1e5c94941a53
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-10-10 21:18:00 +0200

    Add Cache class, switch to memcached.

--- lib/cache.php
+++ lib/cache.php
@@ -1,14 +1,39 @@
 <?php
 
-function hitCache($fingerprint, $uncachedContent) {
-	$hash = sha1(var_export($fingerprint, true));
-	echo var_export($fingerprint, true);
-	$cached = result("SELECT content FROM cache WHERE hash = ?", [$hash]);
-	if ($cached) {
-		return $cached;
-	} else {
-		$content = preg_replace('/\t|(?:\r?\n[ \t]*)+/s', '', $uncachedContent());
-		query("INSERT INTO cache (hash, content) VALUES (?,?)", [$hash, $content]);
-		return $content;
+class Cache {
+	private $enabled = false;
+	private $memcached;
+
+	function __construct($memcachedServers) {
+		if (!empty($memcachedServers)) {
+			$this->enabled = true;
+			$this->memcached = new Memcached();
+			foreach ($memcachedServers as $memcachedServer) {
+				$this->memcached->addServer($memcachedServer, 11211);
+			}
+		}
+	}
+
+	public function hit($fingerprint, $uncachedContent) {
+		if ($this->enabled) {
+			return $this->hitMem($fingerprint, $uncachedContent);
+		} else {
+			return $uncachedContent();
+		}
+	}
+
+	private function hitMem($fingerprint, $uncachedContent) {
+		// TODO: Switch to xxHash when PHP 8.1 releases.
+		$hash = sha1(var_export($fingerprint, true));
+		$cached = $this->memcached->get($hash);
+		if ($cached) {
+			return $cached;
+		} else {
+			$content = $uncachedContent();
+			$this->memcached->set($hash, $content);
+			return $content;
+		}
 	}
 }
+
+$cache = new Cache($memcachedServers);

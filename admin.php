e37cd0eaa35f30d674a9fb44db2e2f9b6d8cd397
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-10-13 21:54:19 +0200

    Start some cool memcached statistics table...

--- admin.php
+++ admin.php
@@ -1,5 +1,15 @@
 <?php
 require('lib/common.php');
 
+$memcachedStats = $cache->memcached->getStats();
+
+/*
+echo '<pre>';
+print_r($memcachedStats);
+echo '</pre>';
+*/
+
 $twig = twigloader();
-echo $twig->render('admin.twig');
+echo $twig->render('admin.twig', [
+	'memcached_stats' => $memcachedStats
+]);

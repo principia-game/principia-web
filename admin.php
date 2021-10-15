e9a37ec3e46d73204e31b22082b85b7351a9da7f
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-10-15 10:26:27 +0000

    Add list showing last 7 registered and seen users.

--- admin.php
+++ admin.php
@@ -9,9 +9,15 @@ print_r($memcachedStats);
 echo '</pre>';
 */
 
+$latestRegisteredUsers = query("SELECT id, name, customcolor, joined FROM users ORDER BY joined DESC LIMIT 7");
+$latestSeenUsers = query("SELECT id, name, customcolor, lastview FROM users ORDER BY lastview DESC LIMIT 7");
+
+
 $memcachedStats['dummy-server.lol'] = ['offline' => true];
 
 $twig = twigloader();
 echo $twig->render('admin.twig', [
-	'memcached_stats' => $memcachedStats
+	'memcached_stats' => $memcachedStats,
+	'latest_registered_users' => $latestRegisteredUsers,
+	'latest_seen_users' => $latestSeenUsers
 ]);

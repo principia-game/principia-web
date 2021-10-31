03b36ad77f75a6b534d341a725c1f9d2a80abc49
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-10-31 22:49:56 +0100

    Cache all index page queries into memcached.

--- index.php
+++ index.php
@@ -1,11 +1,21 @@
 <?php
 require('lib/common.php');
 
-$newsdata = query("SELECT * FROM news ORDER BY id DESC LIMIT 5");
+// Cache all index page queries for 1 hour unless explicitly flushed.
+
+$newsdata = $cache->hit('idx_news', function () {
+	return fetchArray(query("SELECT * FROM news ORDER BY id DESC LIMIT 5"));
+}, 60*60);
 
 $latestquery = "SELECT $userfields l.id id,l.title title,l.locked locked FROM levels l JOIN users u ON l.author = u.id WHERE l.cat = %d AND l.locked = 0 ORDER BY l.id DESC LIMIT 4";
-$latestcustom = query(sprintf($latestquery, 1));
-$latestadvent = query(sprintf($latestquery, 2));
+
+$latestcustom = $cache->hit('idx_anp', function () use ($latestquery) {
+	return fetchArray(query(sprintf($latestquery, 1)));
+}, 60*60);
+
+$latestadvent = $cache->hit('idx_adv', function () use ($latestquery) {
+	return fetchArray(query(sprintf($latestquery, 2)));
+}, 60*60);
 
 $justRegistered = (isset($_GET['rd']) ? true : false);
 
@@ -13,7 +23,7 @@ $twig = twigloader();
 
 echo $twig->render('index.twig', [
 	'just_registered' => $justRegistered,
-	'news' => fetchArray($newsdata),
-	'custom_levels' => fetchArray($latestcustom),
-	'adventure_levels' => fetchArray($latestadvent)
+	'news' => $newsdata,
+	'custom_levels' => $latestcustom,
+	'adventure_levels' => $latestadvent
 ]);

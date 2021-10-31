dc543847b9affd631f1788ba012bfbfdcddf785c
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-10-31 22:11:59 +0100

    Add some table counts to admin panel.

--- admin.php
+++ admin.php
@@ -5,15 +5,17 @@ if ($userdata['powerlevel'] < 3) error('403', "You shouldn't be here, get out!")
 
 $memcachedStats = $cache->memcached->getStats();
 
-/*
-echo '<pre>';
-print_r($memcachedStats);
-echo '</pre>';
-*/
-
 $latestRegisteredUsers = query("SELECT id, name, customcolor, joined FROM users ORDER BY joined DESC LIMIT 7");
 $latestSeenUsers = query("SELECT id, name, customcolor, lastview FROM users ORDER BY lastview DESC LIMIT 7");
 
+$thingsToCount = ['levels', 'packages', 'comments', 'users'];
+
+$query = "SELECT ";
+foreach ($thingsToCount as $thing) {
+	if ($query != "SELECT ") $query .= ", ";
+	$query .= sprintf("(SELECT COUNT(*) FROM %s) %s", $thing, $thing);
+}
+$count = fetch($query);
 
 $memcachedStats['dummy-server.lol'] = ['offline' => true];
 
@@ -21,5 +23,7 @@ $twig = twigloader();
 echo $twig->render('admin.twig', [
 	'memcached_stats' => $memcachedStats,
 	'latest_registered_users' => $latestRegisteredUsers,
-	'latest_seen_users' => $latestSeenUsers
+	'latest_seen_users' => $latestSeenUsers,
+	'things_to_count' => $thingsToCount,
+	'count' => $count,
 ]);

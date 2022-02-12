945b4d70651cd45ed28885a9f0cda63f4488df70
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-02-12 13:35:51 +0100

    Inline fonts to allow faster independent loading.

--- tools/compile-scss-sassc.sh
+++ tools/compile-scss-sassc.sh
@@ -2,5 +2,6 @@
 
 common_arguments="--style compressed --load-path ./"
 
-sassc ${common_arguments} assets/scss/style.scss assets/css/style.css
 sassc ${common_arguments} assets/scss/darkmode.scss assets/css/darkmode.css
+sassc ${common_arguments} assets/scss/fonts.scss assets/css/fonts.css
+sassc ${common_arguments} assets/scss/style.scss assets/css/style.css

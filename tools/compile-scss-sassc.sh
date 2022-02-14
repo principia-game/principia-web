3be5470c23d5224df20c32a97aa430889eb1f321
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-02-14 20:04:32 +0100

    Cache compressed versions of compiled stylesheets.

--- tools/compile-scss-sassc.sh
+++ tools/compile-scss-sassc.sh
@@ -5,3 +5,7 @@ common_arguments="--style compressed --load-path ./"
 sassc ${common_arguments} assets/scss/darkmode.scss assets/css/darkmode.css
 sassc ${common_arguments} assets/scss/fonts.scss assets/css/fonts.css
 sassc ${common_arguments} assets/scss/style.scss assets/css/style.css
+
+# Compress compiled stylesheets for nginx gzip_static
+gzip assets/css/darkmode.css
+gzip assets/css/style.css

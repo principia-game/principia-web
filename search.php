89a1c503b03d53440d5edefda72e75851f786da9
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-03-06 21:03:15 +0100

    Don't run search queries if nothing has been entered.

--- search.php
+++ search.php
@@ -1,19 +1,21 @@
 <?php
 require('lib/common.php');
 
-$query = (isset($_GET['query']) ? $_GET['query'] : '');
+$query = (isset($_GET['query']) ? trim($_GET['query']) : '');
 $page = (isset($_GET['page']) && is_numeric($_GET['page']) && $_GET['page'] > 0 ? $_GET['page'] : 1);
 
-$limit = sprintf("LIMIT %s,%s", (($page - 1) * $lpp), $lpp);
-$levels = query("SELECT $userfields l.id id,l.title title,l.locked locked FROM levels l JOIN users u ON l.author = u.id WHERE l.title LIKE CONCAT('%', ?, '%') AND l.locked = 0 ORDER BY l.id DESC $limit",
-	[$query]);
-$count = result("SELECT COUNT(*) FROM levels l WHERE title LIKE CONCAT('%', ?, '%')",
-	[$query]);
+if ($query) {
+	$limit = sprintf("LIMIT %s,%s", (($page - 1) * $lpp), $lpp);
+	$levels = query("SELECT $userfields l.id id,l.title title,l.locked locked FROM levels l JOIN users u ON l.author = u.id WHERE l.title LIKE CONCAT('%', ?, '%') AND l.locked = 0 ORDER BY l.id DESC $limit",
+		[$query]);
+	$count = result("SELECT COUNT(*) FROM levels l WHERE title LIKE CONCAT('%', ?, '%')",
+		[$query]);
+}
 
 $twig = twigloader();
 echo $twig->render('search.twig', [
 	'query' => $query,
 	'page' => $page,
-	'levels' => $levels,
-	'level_count' => $count
+	'levels' => (isset($levels) ? $levels : ''),
+	'level_count' => (isset($count) ? $count : '')
 ]);

50a3b772d30495cde4817586dd2073186d3ac0b0
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-04-11 12:49:01 +0200

    Fix abusive registration checks being completely broken.

--- internal/android_register.php
+++ internal/android_register.php
@@ -4,7 +4,7 @@ require('lib/common.php');
 
 if (!isset($_POST['signature']) || !isset($_POST['userdata'])) die('nah');
 
-$name = $_POST['username'] ?? null;
+$name = trim($_POST['username'] ?? null);
 // Yes... These variables are mixed up. This is intentional because Emil, in his infinite wisdom,
 //		  thought that putting the password in 'email' and email in 'password' was a good idea.
 $mail = $_POST['password'] ?? null;
@@ -13,13 +13,26 @@ $pass = $_POST['email'] ?? null;
 // TODO: code 117 - This email has been banned.
 //       code 118 is useless to us as we're not checking your license
 
-if (!isset($name)) die('111'); // 111 is a generic "something went wrong!" code.
-if (!isset($mail) || !filter_var($mail, FILTER_VALIDATE_EMAIL)) die('115'); // "This email is invalid."
-if (!isset($pass) || strlen($pass) < 6) die('114'); // "The password is invalid."
-if (result("SELECT COUNT(*) FROM users WHERE name = ?", [$name])) die('112'); // "This username is already taken."
-if (!preg_match('/[a-zA-Z0-9_]+$/', $name)) die('113'); // "The username contains invalid characters."
-if (result("SELECT COUNT(*) FROM users WHERE email = ?", [$mail])) die('116'); // "This email is already in use."
-if (result("SELECT COUNT(*) FROM users WHERE ip = ?", [$_SERVER['REMOTE_ADDR']])) die('111'); // Just give the generic "something went wrong!" code.
+if (!isset($name))
+	die('111'); // 111 is a generic "something went wrong!" code.
+
+if (!isset($mail) || !filter_var($mail, FILTER_VALIDATE_EMAIL))
+	die('115'); // "This email is invalid."
+
+if (!isset($pass) || strlen($pass) < 6)
+	die('114'); // "The password is invalid."
+
+if (result("SELECT COUNT(*) FROM users WHERE LOWER(name) = ?", [strtolower($name)]))
+	die('112'); // "This username is already taken."
+
+if (!preg_match('/[a-zA-Z0-9_]+$/', $name))
+	die('113'); // "The username contains invalid characters."
+
+if (result("SELECT COUNT(*) FROM users WHERE email = ?", [$mail]))
+	die('116'); // "This email is already in use."
+
+if (result("SELECT COUNT(*) FROM users WHERE ip = ?", [$_SERVER['REMOTE_ADDR']]))
+	die('111'); // Just give the generic "something went wrong!" code.
 
 // All possible invalid credentials have been checked, it should be successful now.
 register($name, $pass, $mail);
--- register.php
+++ register.php
@@ -6,23 +6,42 @@ if ($log) redirect('./');
 $error = '';
 
 if (isset($_POST['action'])) {
-	$name = $_POST['name'] ?? null;
+	$name = trim($_POST['name'] ?? null);
 	$mail = $_POST['mail'] ?? null;
 	$pass = $_POST['pass'] ?? null;
 	$pass2 = $_POST['pass2'] ?? null;
 	$captchaId = (isset($_POST['uwu']) && isset($captcha[$_POST['uwu']]) ? $_POST['uwu'] : null);
 	$captchaAnswer = $_POST['jupiter'] ?? null;
 
-	if (!isset($name)) $error .= 'Blank username. ';
-	if (!isset($mail)) $error .= 'Blank email. ';
-	if (!isset($pass) || strlen($pass) < 6) $error .= 'Password is too short. ';
-	if (!isset($pass2) || $pass != $pass2) $error .= "The passwords don't match. ";
-	if (result("SELECT COUNT(*) FROM users WHERE name = ?", [$name])) $error .= "Username has already been taken. ";
-	if (!in_array(strtolower($captchaAnswer), $captcha[$captchaId]['answer'])) $error .= "Wrong security question answer. ";
-	if (!preg_match('/[a-zA-Z0-9_]+$/', $name)) $error .= "Username contains invalid characters (Only alphanumeric and underscore allowed). ";
-	if (!filter_var($mail, FILTER_VALIDATE_EMAIL)) $error .= "Email isn't valid. ";
-	if (result("SELECT COUNT(*) FROM users WHERE email = ?", [$mail])) $error .= "You've already registered an account using this email address. ";
-	if (result("SELECT COUNT(*) FROM users WHERE ip = ?", [$_SERVER['REMOTE_ADDR']])) $error .= "Creating multiple accounts (alts) aren't allowed. ";
+	if (!isset($name))
+		$error .= 'Blank username. ';
+
+	if (!isset($mail))
+		$error .= 'Blank email. ';
+
+	if (!isset($pass) || strlen($pass) < 6)
+		$error .= 'Password is too short. ';
+
+	if (!isset($pass2) || $pass != $pass2)
+		$error .= "The passwords don't match. ";
+
+	if (result("SELECT COUNT(*) FROM users WHERE LOWER(name) = ?", [strtolower($name)]))
+		$error .= "Username has already been taken. ";
+
+	if (!in_array(strtolower($captchaAnswer), $captcha[$captchaId]['answer']))
+		$error .= "Wrong security question answer. ";
+
+	if (!preg_match('/[a-zA-Z0-9_]+$/', $name))
+		$error .= "Username contains invalid characters (Only alphanumeric and underscore allowed). ";
+
+	if (!filter_var($mail, FILTER_VALIDATE_EMAIL))
+		$error .= "Email isn't valid. ";
+
+	if (result("SELECT COUNT(*) FROM users WHERE email = ?", [$mail]))
+		$error .= "You've already registered an account using this email address. ";
+
+	if (result("SELECT COUNT(*) FROM users WHERE ip = ?", [$_SERVER['REMOTE_ADDR']]))
+		$error .= "Creating multiple accounts (alts) aren't allowed. ";
 
 	if ($error == '') {
 		$token = register($name, $pass, $mail);

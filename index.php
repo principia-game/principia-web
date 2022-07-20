f4fb4d617e827f1da183e951ed5fb0dd4dadd932
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-07-20 15:17:05 +0200

    Index page improvements, add top levels and the date for news

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <?php
 require('lib/common.php');
 
-// Cache all index page queries for 1 hour unless explicitly flushed.
+// Cache all index page queries unless explicitly flushed.
+
+$latestfeatured = $cache->hit('idx_feat', function () use ($userfields) {
+	return fetchArray(query("SELECT $userfields l.id id,l.title title,l.locked locked FROM featured f JOIN levels l on f.level = l.id JOIN users u ON l.author = u.id ORDER BY f.id DESC LIMIT 4"));
+});
 
 $newsdata = $cache->hit('idx_news', function () {
-	return fetchArray(query("SELECT id, title FROM news ORDER BY id DESC LIMIT 5"));
+	return fetchArray(query("SELECT id, title, time FROM news ORDER BY id DESC LIMIT 5"));
+});
+
+$toplevels = $cache->hit('idx_top', function () use ($userfields) {
+	return fetchArray(query("SELECT $userfields l.id id,l.title title,l.locked locked FROM levels l JOIN users u ON l.author = u.id WHERE l.locked = 0 ORDER BY l.likes DESC, l.id DESC LIMIT 4"));
 });
 
 $latestquery = "SELECT $userfields l.id id,l.title title,l.locked locked FROM levels l JOIN users u ON l.author = u.id WHERE l.cat = %d AND l.locked = 0 ORDER BY l.id DESC LIMIT 4";
@@ -17,18 +25,15 @@ $latestadvent = $cache->hit('idx_adv', function () use ($latestquery) {
 	return fetchArray(query(sprintf($latestquery, 2)));
 });
 
-$latestfeatured = $cache->hit('idx_feat', function () use ($userfields) {
-	return fetchArray(query("SELECT $userfields l.id id,l.title title,l.locked locked FROM featured f JOIN levels l on f.level = l.id JOIN users u ON l.author = u.id ORDER BY f.id DESC LIMIT 4"));
-});
-
 $justRegistered = (isset($_GET['rd']) ? true : false);
 
 $twig = twigloader();
 
 echo $twig->render('index.twig', [
 	'just_registered' => $justRegistered,
+	'featured_levels' => $latestfeatured,
 	'news' => $newsdata,
+	'top_levels' => $toplevels,
 	'custom_levels' => $latestcustom,
 	'adventure_levels' => $latestadvent,
-	'featured_levels' => $latestfeatured
 ]);

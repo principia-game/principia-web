0c209ba84005131a466f7503827c447f5ad40025
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-08-09 23:06:30 +0200

    Less strain on the server with repeated PVC requests

--- lib/common.php
+++ lib/common.php
@@ -88,11 +88,13 @@ if ($log) {
 	$userdata = fetch("SELECT * FROM users WHERE id = ?", [$id]);
 	$notificationCount = result("SELECT COUNT(*) FROM notifications WHERE recipient = ?", [$userdata['id']]);
 
-	if ($userdata['powerlevel'] < 0) {
-		$userdata['banreason'] = result("SELECT reason FROM bans WHERE user = ?", [$id]);
-	}
+	if (!str_contains($_SERVER['SCRIPT_NAME'], 'internal')) {
+		if ($userdata['powerlevel'] < 0) {
+			$userdata['banreason'] = result("SELECT reason FROM bans WHERE user = ?", [$id]);
+		}
 
-	query("UPDATE users SET lastview = ?, ip = ? WHERE id = ?", [time(), $ipaddr, $userdata['id']]);
+		query("UPDATE users SET lastview = ?, ip = ? WHERE id = ?", [time(), $ipaddr, $userdata['id']]);
+	}
 } else {
 	$userdata['powerlevel'] = 0;
 	$userdata['darkmode'] = $darkModeDefault;

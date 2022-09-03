999f31eeac70628a3ccedb2375e107cb57797607
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-09-03 23:39:16 +0200

    Add helper function to toggle visibility of an element

--- assets/base.js
+++ assets/base.js
@@ -8,6 +8,21 @@ function vote(el, id) {
 	el.disabled = 'disabled';
 }
 
+// toggle visibility of element with id `id`,
+// reflecting the status in id `feedback`.
+function toggleVis(id, feedback) {
+	// descriptive variables yes
+	var x = document.getElementById(id);
+	var y = document.getElementById(feedback);
+
+	if (x.style.display == "none") {
+		x.style.display = "block";
+		y.innerHTML = 'Hide';
+	} else {
+		x.style.display = "none";
+		y.innerHTML = 'Show';
+	}
+}
 
 // Forum thread.php code
 function submitmod(act) {

6af512b6b13389661e33e7f8eb426471d9657f6f
ROllerozxa <rollerozxa@voxelmanip.se>
2022-10-12 21:54:40 +0200

    Update gource-source-tree to include the wiki and an old copy of the forum repo

--- tools/gource-source-tree.sh
+++ tools/gource-source-tree.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
-# Start a gource visualisation with the commit history of principia-web and the forums combined.
+# Start a gource visualisation with the commit history of principia-web along with the (old) forum repo and wiki.
 
 gource --output-custom-log /tmp/gource_principia-web.log .
-gource --output-custom-log /tmp/gource_principia-web-forum.log forum/
+
+# Forum repo (running from old copy of forum repo)
+gource --output-custom-log /tmp/gource_principia-web-forum.log ../principia-web_misc/forum/
 sed -i -r "s#(.+)\|#\1|/forum#" /tmp/gource_principia-web-forum.log
 
-cat /tmp/gource_principia-web.log /tmp/gource_principia-web-forum.log | sort -n | gource --key -1280x720 $@
+# Wiki repo
+gource --output-custom-log /tmp/gource_principia-web-wiki.log wiki/
+sed -i -r "s#(.+)\|#\1|/wiki#" /tmp/gource_principia-web-wiki.log
+
+# Concatenate them together
+cat /tmp/gource_principia-web{,-forum,-wiki}.log | sort -n | gource - --log-format custom --key -1280x720 $@

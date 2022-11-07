3341f09a1df80a555c72d3b75809eaf67381b026
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-11-07 13:50:49 +0100

    Show message if not logged in, temporary fix for submitting highscore on game over

--- internal/submit_score.php
+++ internal/submit_score.php
@@ -8,13 +8,21 @@ $progfile = $_FILES['data_bin'];
 
 if (!$level || !$progfile || $progfile['name'] != 'data.bin') die("Cheatin'?");
 
+if (!$log) {
+	header("X-Error-Message: You need to be signed in to submit your score.");
+	header("X-Error-Action: 1"); // Display login dialog
+	die();
+}
+
 $output = exec(sprintf(
 	'./tools/progress-get %s %d',
 $progfile['tmp_name'], $level));
 
 $result = explode(',', $output);
 
-if ($result[0] == 1) {
+// TODO: Completion doesn't necessary mean you're eligble for highscore submission,
+// see "Submit on game over" level property. Will need database-level level flags at some point.
+//if ($result[0] == 1) {
 	$lastscore = result("SELECT score FROM leaderboard WHERE level = ? AND user = ?", [$level, $userdata['id']]);
 
 	if ($lastscore === false) {
@@ -32,4 +40,4 @@ if ($result[0] == 1) {
 			header('x-notify-message: You already have a highscore better than this.');
 		}
 	}
-}
+//}

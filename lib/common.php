0a7caaf99218963203f2758575b531e9cd226724
ROllerozxa <rollerozxa@voxelmanip.se>
2023-02-13 17:12:55 +0100

    Add some security headers

--- lib/common.php
+++ lib/common.php
@@ -13,6 +13,18 @@ foreach (glob("lib/*.php") as $file) {
 	require_once($file);
 }
 
+// Security headers.
+header("Content-Security-Policy:"
+	."default-src 'self';"
+	."script-src 'self' 'unsafe-inline';"
+	."img-src 'self' data: *.voxelmanip.se voxelmanip.se *.imgur.com imgur.com *.github.com github.com *.githubusercontent.com;"
+	."frame-src *.youtube-nocookie.com;"
+	."style-src 'self' 'unsafe-inline';");
+header("Referrer-Policy: strict-origin-when-cross-origin");
+header("X-Content-Type-Options: nosniff");
+header("X-Frame-Options: SAMEORIGIN");
+header("X-Xss-Protection: 1; mode=block");
+
 if (!isset($acmlm))
 	$userfields = userfields();
 

59ce3c8c74a62a0a625ba577bb5a4d6de89c1b02
ROllerozxa <rollerozxa@voxelmanip.se>
2023-02-14 19:20:26 +0100

    Add voxelmanip.se to media-src (required for intro video)

--- lib/common.php
+++ lib/common.php
@@ -13,17 +13,21 @@ foreach (glob("lib/*.php") as $file) {
 	require_once($file);
 }
 
-// Security headers.
-header("Content-Security-Policy:"
-	."default-src 'self';"
-	."script-src 'self' 'unsafe-inline';"
-	."img-src 'self' data: *.voxelmanip.se voxelmanip.se *.imgur.com imgur.com *.github.com github.com *.githubusercontent.com;"
-	."frame-src *.youtube-nocookie.com;"
-	."style-src 'self' 'unsafe-inline';");
-header("Referrer-Policy: strict-origin-when-cross-origin");
-header("X-Content-Type-Options: nosniff");
-header("X-Frame-Options: SAMEORIGIN");
-header("X-Xss-Protection: 1; mode=block");
+if (!str_contains($_SERVER['SCRIPT_NAME'], 'internal')) {
+	// Security headers.
+	header("Content-Security-Policy:"
+		."default-src 'self';"
+		."script-src 'self' 'unsafe-inline';"
+		."img-src 'self' data: *.voxelmanip.se voxelmanip.se *.imgur.com imgur.com *.github.com github.com *.githubusercontent.com;"
+		."media-src 'self' *.voxelmanip.se voxelmanip.se;"
+		."frame-src *.youtube-nocookie.com;"
+		."style-src 'self' 'unsafe-inline';");
+
+	header("Referrer-Policy: strict-origin-when-cross-origin");
+	header("X-Content-Type-Options: nosniff");
+	header("X-Frame-Options: SAMEORIGIN");
+	header("X-Xss-Protection: 1; mode=block");
+}
 
 if (!isset($acmlm))
 	$userfields = userfields();

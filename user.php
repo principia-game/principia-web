dea4c7429974e6b9819d2bef23210c0f6652865e
ROllerozxa <rollerozxa@voxelmanip.se>
2023-03-30 22:53:25 +0200

    Large user page rework, minor settings page rework

--- user.php
+++ user.php
@@ -83,6 +83,8 @@ if (isset($userdata['id']) && $userdata['id'] == $id && !$forceuser) {
 		return result("SELECT COUNT(*) FROM levels l WHERE l.author = ? AND l.visibility = 0", [$id]);
 	});
 
+	$wikiconts = result("SELECT COUNT(*) FROM wikirevisions WHERE author = ?", [$id]);
+
 	$comments = query("SELECT $userfields c.* FROM comments c JOIN users u ON c.author = u.id WHERE c.type = 4 AND c.level = ? ORDER BY c.time DESC", [$id]);
 
 	if (isset($userdata['id']) && $id == $userdata['id'])
@@ -99,6 +101,7 @@ if (isset($userdata['id']) && $userdata['id'] == $id && !$forceuser) {
 		'page' => $page,
 		'level_count' => $count,
 		'action' => $action ?? null,
-		'comments' => $comments ?? null
+		'comments' => $comments ?? null,
+		'wikiconts' => $wikiconts
 	]);
 }

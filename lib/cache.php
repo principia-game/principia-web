5abca94054ff3ceca4f14c63b0426c458b1a6c1c
ROllerozxa <rollerozxa@voxelmanip.se>
2023-08-13 23:42:41 +0200

    Split up Cache class into generic Cache trait and store-specific classes

--- lib/cache.php
+++ lib/cache.php
@@ -1,24 +1,72 @@
 <?php
 
-assert(apcu_enabled());
-
-class Cache {
+/**
+ * Cache trait, contains all caching-system-agnostic code.
+ *
+ * Classes using this trait need to implement the following methods:
+ * - get($key)
+ * - set($key, $value, $expiration = 0)
+ * - delete($key)
+ */
+trait Cache {
 	public function hit($key, $uncachedContent, $expire = 0) {
 		return $this->hitMem($key, $uncachedContent, false, $expire);
 	}
 
+	public function hitHash($fingerprint, $uncachedContent, $expire = 0) {
+		$fingerprint = hash("xxh128", var_export($fingerprint, true));
+		return $this->hitMem($fingerprint, $uncachedContent, true, $expire);
+	}
+
 	private function hitMem($key, $uncachedContent, $expire = 0) {
-		$cached = apcu_fetch($key);
+		$cached = $this->get($key);
 		if ($cached !== false) {
 			return $cached;
 		} else {
 			$content = $uncachedContent();
-			apcu_store($key, $content, $expire);
+			$this->set($key, $content, $expire);
 			return $content;
 		}
 	}
 
-	// Low-level wrapper functions
+	public function deleteHash($fingerprint) {
+		$hash = hash("xxh128", var_export($fingerprint, true));
+		$this->delete($hash);
+	}
+}
+
+/**
+ * Implements a caching class using memcached.
+ */
+class CacheMemcached {
+	use Cache;
+
+	public $memcached;
+
+	function __construct($memcachedServers) {
+		$this->memcached = new Memcached();
+		$this->memcached->addServers($memcachedServers);
+	}
+
+	public function get($key) {
+		return $this->memcached->get($key);
+	}
+
+	public function set($key, $value, $expiration = 0) {
+		return $this->memcached->set($key, $value, $expiration);
+	}
+
+	public function delete($key) {
+		return $this->memcached->delete($key);
+	}
+}
+
+/**
+ * Implements a caching class using APCu.
+ */
+class CacheAPCu {
+	use Cache;
+
 	public function get($key) {
 		return apcu_fetch($key);
 	}
@@ -32,4 +80,9 @@ class Cache {
 	}
 }
 
-$cache = new Cache();
+// Check that APCu is actually enabled.
+assert(apcu_enabled());
+
+$cache = new CacheAPCu();
+
+//require('lib/benchmark/cache.php');

8a5ea07166a5bc3b2094a5de3c025385477923a7
ROllerozxa <rollerozxa@voxelmanip.se>
2023-09-01 20:22:44 +0200

    Make profiler class more reusable

--- lib/profiler.php
+++ lib/profiler.php
@@ -4,10 +4,22 @@ class Profiler {
 	private $starttime;
 
 	function __construct() {
+		$this->resetTime();
+	}
+
+	function resetTime() {
 		$this->starttime = microtime(true);
 	}
 
 	function getStats() {
-		printf("Rendered in %1.3fs with %dKB memory used", microtime(true) - $this->starttime, memory_get_usage(false) / 1024);
+		printf("Rendered in %1.3fs with %dKB memory used", $this->getTime(), $this->getMemUsage());
+	}
+
+	function getTime() {
+		return microtime(true) - $this->starttime;
+	}
+
+	function getMemUsage() {
+		return memory_get_usage(false) / 1024;
 	}
 }

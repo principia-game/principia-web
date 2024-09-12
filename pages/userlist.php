2dd1861ffb85d645ed33fa7eae2dcb2a16fdaaac
ROllerozxa <rollerozxa@voxelmanip.se>
2024-09-12 20:23:00 +0200

    Fix userlist level counts when user IDs are missing

--- pages/userlist.php
+++ pages/userlist.php
@@ -2,9 +2,9 @@
 
 $users = query("SELECT id, name, customcolor, joined, rank FROM users");
 
-$ucount = result("SELECT COUNT(*) FROM users");
+$maxid = result("SELECT MAX(id) FROM users");
 $levelcounts = [];
-for ($i = 1; $i <= $ucount; $i++) {
+for ($i = 1; $i <= $maxid; $i++) {
 	$levelcounts[$i] = $cache->hit('levelcount_'.$i, function () use ($i) {
 		return result("SELECT COUNT(*) FROM levels l WHERE l.author = ? AND l.visibility = 0", [$i]);
 	});

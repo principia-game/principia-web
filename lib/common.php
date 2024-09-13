4613745382eb06b625208b7bcdb4aaf1ae88a1c7
ROllerozxa <rollerozxa@voxelmanip.se>
2024-09-13 23:51:29 +0200

    Update CSP image whitelist

--- lib/common.php
+++ lib/common.php
@@ -26,7 +26,7 @@ if (!$internal) {
 	header("Content-Security-Policy:"
 		."default-src 'self';"
 		."script-src 'self' 'unsafe-inline';"
-		."img-src 'self' data: *.principia-web.se principia-web.se *.voxelmanip.se voxelmanip.se *.imgur.com imgur.com *.github.com github.com *.githubusercontent.com cdn.discordapp.com;"
+		."img-src 'self' data: *.principia-web.se principia-web.se *.voxelmanip.se voxelmanip.se *.imgur.com imgur.com *.github.com github.com *.githubusercontent.com *.postimg.cc postimg.cc;"
 		."media-src 'self' *.voxelmanip.se voxelmanip.se;"
 		."frame-src *.youtube-nocookie.com itch.io;"
 		."style-src 'self' 'unsafe-inline';");

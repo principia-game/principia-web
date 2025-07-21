f952f3bc4ebf554bffb2530ac221efb2897de009
ROllerozxa <rollerozxa@voxelmanip.se>
2025-07-21 19:38:36 +0200

    Add GeoIP functionality

---
+++ lib/geoip.php
@@ -0,0 +1,9 @@
+<?php
+
+$geoip = new MaxMind\Db\Reader('data/GeoLite2-Country.mmdb');
+
+function getUserCountry() {
+	global $geoip, $ipaddr;
+
+	return $geoip->get($ipaddr)['country']['iso_code'];
+}

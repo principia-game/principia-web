b19a438d2cfc74941c0a4ffb9de075121a286c7c
ROllerozxa <rollerozxa@voxelmanip.se>
2025-10-06 00:01:49 +0200

    Fix viewlocked page

--- pages/viewlocked.php
+++ pages/viewlocked.php
@@ -3,7 +3,7 @@
 if (!IS_ARCHIVE && !IS_MOD) error('403');
 
 $lockedlevels = query("SELECT l.id, l.title, l.visibility, @userfields
-		FROM levels l JOIN users u ON l.author = u.id WHERE l.visibility = 1 ORDER BY l.id DESC");
+		FROM @levels l JOIN @users u ON l.author = u.id WHERE l.visibility = 1 ORDER BY l.id DESC");
 
 twigloader()->display('viewlocked.twig', [
 	'levels' => fetchArray($lockedlevels)

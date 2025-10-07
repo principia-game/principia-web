dc747baf602e97dc469720b88ae5d72e9dcc324a
ROllerozxa <rollerozxa@voxelmanip.se>
2025-10-07 14:38:55 +0200

    Make archive subdomain redirects permanent

--- lib/routes/legacy_archive.php
+++ lib/routes/legacy_archive.php
@@ -10,12 +10,12 @@ const DOMAIN = 'https://principia-web.se';
 
 if (isset($path[1]) && $path[1] != '') {
 	if (in_array($path[1], ['latest', 'level', 'popular', 'random', 'search', 'sitemap', 'top', 'user', 'userlist'])) {
-		redirect(DOMAIN.'/archive%s', $_SERVER["REQUEST_URI"]);
+		redirectPerma(DOMAIN.'/archive%s', $_SERVER["REQUEST_URI"]);
 	} elseif ($path[1] == 'internal' && in_array($path[2], ['get_level', 'derive_level', 'edit_level'])) {
 		redirect(DOMAIN.'/', $_SERVER["REQUEST_URI"]);
 	} else {
 		http_response_code(404);
-		die('404');
+		die('<title>404 Not Found</title><center><h1>404 Not Found</h1><hr>principia-web</center>');
 	}
 } else
-	redirect(DOMAIN.'/archive/');
+	redirectPerma(DOMAIN.'/archive/');

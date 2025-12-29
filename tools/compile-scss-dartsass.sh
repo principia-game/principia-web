60bf9d9c1390e72cdf23df54dc1f482051ced162
ROllerozxa <rollerozxa@voxelmanip.se>
2025-12-29 17:25:38 +0100

    Fix dart sass compilation, move out rest of the styling

--- tools/compile-scss-dartsass.sh
+++ tools/compile-scss-dartsass.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-sass --style=compressed --no-source-map --watch scss/:static/css/
+sass --style=compressed --no-source-map --watch \
+	scss/adminer.scss:static/css/adminer.css \
+	scss/dark.scss:static/css/dark.css \
+	scss/fonts.scss:static/css/fonts.css \
+	scss/light.scss:static/css/light.css
